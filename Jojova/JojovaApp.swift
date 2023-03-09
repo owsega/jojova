9ce00de14149b6fd23089501ffe78247d03883ad
Gabriel Owoeye <gabriel.owoeye@paramount.com>
2023-03-09 20:00:38 +0100

    Initial Commit

---
+++ Jojova/JojovaApp.swift
@@ -0,0 +1,17 @@
+//
+//  JojovaApp.swift
+//  Jojova
+//
+//  Created by Gabriel Owoeye on 09/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct JojovaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
